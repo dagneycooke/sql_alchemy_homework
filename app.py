9d1f77f00d705df815641d952f388f541c05cac2
Dagney Cooke <14912982+dagneycooke@users.noreply.github.com>
2020-02-06 23:20:44 -0800

    homework

--- app.py
+++ app.py
@@ -32,23 +32,29 @@ app = Flask(__name__)
 # home page
 @app.route("/")
 def welcome():
-    """List all available api routes."""
+
+ #   List all available routes
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/YYYY-MM-DD"
+        f"/api/v1.0/YYYY-MM-DD/YYYY-MM-DD"
+
     )
 
+# create route for precipitation data
 @app.route("/api/v1.0/precipitation")
 def passengers():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
     """Return a list of measurement data including the date and prcp"""
-    # Query all passengers
+    # Query all measurement dates and precipitation
     results = session.query(Measurement.date, Measurement.prcp).all()
 
+    # close sessio
     session.close()
 
     #Convert the query results to a Dictionary using date as the key and prcp as the value.
@@ -58,6 +64,7 @@ def passengers():
         dates_dict[date] = prcp
         all_dates.append(dates_dict)
 
+    # return json version of dates/precipitation data
     return jsonify(all_dates)
 
 # display all station names
@@ -99,14 +106,22 @@ def tobs():
         filter(Measurement.date >= one_year_ago).all()
     session.close()
 
-    # Convert list of tuples into normal list
-    tobs_list = list(np.ravel(results))
+    # turn list of tuples into normal list
+    tobs_list = []
+    for date, tobs in results:
+        tobs_dict = {}
+        tobs_dict[date] = tobs
+        tobs_list.append(tobs_dict)
 
+    # return json version of dates/precipitation data
     return jsonify(tobs_list)
 
+
+# take start date and get average info since that date
 @app.route("/api/v1.0/<start>")
 def start_only(start): #function provided in starter jupyter notebook
 
+    # open session, query date/measurment info, close session
     session = Session(engine)
 
     results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
@@ -114,16 +129,20 @@ def start_only(start): #function provided in starter jupyter notebook
 
     session.close()
 
+    # convert to list
     temp_list = list(np.ravel(results))
 
+    # convert to dict
     for temp in temp_list:
         temp_dict = {"Min":temp_list[0],"Avg":temp_list[1],"Max":temp_list[2]}
 
     return jsonify(temp_dict)
 
+# take start and end date and get average info for date range
 @app.route("/api/v1.0/<start>/<end>")
 def start_to_end(start,end): #function provided in starter jupyter notebook
 
+    # open session, query measurement info, close session
     session = Session(engine)
 
     results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
@@ -131,18 +150,15 @@ def start_to_end(start,end): #function provided in starter jupyter notebook
 
     session.close()
 
-
+    # convert results to list and then read into dict
     temp_list = list(np.ravel(results))
 
     for temp in temp_list:
         temp_dict = {"Min":temp_list[0],"Avg":temp_list[1],"Max":temp_list[2]}
 
+    # display dict
     return jsonify(temp_dict)
 
-# /api/v1.0/<start> and /api/v1.0/<start>/<end>
-# Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
-# When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
-# When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
 
 if __name__ == '__main__':
     app.run(debug=True)
